1c95bc316dfb348103938c6f22e19c4fb0922538
Koen Bex <koen.bex@innovationstudio.be>
2013-09-21 13:37:42 +0200

    Steppers update labels

--- Medic Kit/WizardPeriodViewController.m
+++ Medic Kit/WizardPeriodViewController.m
@@ -8,10 +8,31 @@
 
 #import "WizardPeriodViewController.h"
 
-@interface WizardPeriodViewController ()
+@interface WizardPeriodViewController () {
+    
+    NSNumber *amountMorning;
+    NSNumber *amountNoon;
+    NSNumber *amountAfternoon;
+    NSNumber *amountEvening;
+    
+    __weak IBOutlet UILabel *morningLabel;
+    __weak IBOutlet UILabel *noonLabel;
+    __weak IBOutlet UILabel *afternoonLabel;
+    __weak IBOutlet UILabel *eveningLabel;
+}
+
 @end
 
 @implementation WizardPeriodViewController
+
+#pragma mark - load
+
+-(void)viewDidLoad {
+    amountMorning = 0;
+    amountNoon = 0;
+    amountAfternoon = 0;
+    amountEvening = 0;
+}
 
 - (void)viewWillAppear:(BOOL)animated {
     
@@ -20,10 +41,33 @@
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(didPressCancel:)];
 }
 
+-(void) reloadLabels {
+    morningLabel.text = [NSString stringWithFormat:@"%@", amountMorning];
+    noonLabel.text = [NSString stringWithFormat:@"%@", amountNoon];
+    afternoonLabel.text = [NSString stringWithFormat:@"%@", amountAfternoon];
+    eveningLabel.text = [NSString stringWithFormat:@"%@", amountEvening];
+}
+
 #pragma mark - Actions
 
 - (IBAction)didPressCancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)morningStep:(UIStepper*)sender {
+    amountMorning = [NSNumber numberWithDouble:[sender value]];
+    [self reloadLabels];
+}
+- (IBAction)noonStep:(UIStepper*)sender {
+    amountNoon = [NSNumber numberWithDouble:[sender value]];
+    [self reloadLabels];
+}
+- (IBAction)afternoonStep:(UIStepper*)sender {
+    amountAfternoon = [NSNumber numberWithDouble:[sender value]];
+    [self reloadLabels];
+}
+- (IBAction)eveningStep:(UIStepper*)sender {
+    amountEvening =  [NSNumber numberWithDouble:[sender value]];
+    [self reloadLabels];
 }
 
 @end
